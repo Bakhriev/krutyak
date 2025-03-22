e0a806b8a4cc4b4a432273d21ce2d136f2d57b86
Bakhriev <legendofpain5112@gmail.com>
2025-03-22 19:12:37 +0500

    tab js

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,40 @@ function setControlsProgress(currentSlide, current, all) {
 }
 
 Fancybox.bind('[data-fancybox="concepts-gallery"]', {});
+
+const tabInit = () => {
+	const tabs = document.querySelectorAll("[data-tab]");
+
+	tabs.forEach(tab => {
+		const btns = tab.querySelectorAll("[data-tab-btn]");
+		const panels = tab.querySelectorAll("[data-tab-panel]");
+
+		btns.forEach(btn => {
+			btn.addEventListener("click", () => {
+				const id = btn.dataset.id;
+
+				toggler(id);
+			});
+		});
+
+		const toggler = id => {
+			btns.forEach(btn => {
+				if (btn.dataset.id === id) {
+					btn.dataset.active = "true";
+				} else {
+					btn.dataset.active = "false";
+				}
+			});
+
+			panels.forEach(panel => {
+				if (panel.dataset.id === id) {
+					panel.dataset.active = "true";
+				} else {
+					panel.dataset.active = "false";
+				}
+			});
+		};
+	});
+};
+
+tabInit();
